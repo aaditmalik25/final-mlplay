05ad998e68d07ad21a11889d2e21db5da30565ff
aaditmalik25 <aaditmalik25@gmail.com>
2025-11-17 10:20:10 +0530

    added 3d

--- src/components/3d/AnimatedBackground.tsx
+++ src/components/3d/AnimatedBackground.tsx
@@ -138,6 +138,7 @@ export function AnimatedBackground() {
 				<div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/20 rounded-full blur-3xl" />
 				<div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-accent/20 rounded-full blur-3xl" />
 			</div>
+			
 		);
 	}
 
